e912cbcb8546e3a2601fd404fe7ee6e0384180bc
Diego Islas <boogeyif@gmail.com>
2024-11-30 15:37:20 -0800

    Created test websocket client that can interact with GCOM. Currently runs as a separate module to send_serial. Once we can test getting drone telemetry, and prove that both modules work we can start integration.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Antenna Tracker Software 2024
+
+## Dependencies
+- python-socketio
+- websocket-client
+- serial
--- send_serial.py
+++ send_serial.py
@@ -1,16 +1,13 @@
-""" File that uses PySerial to send information to the Arduino"""
+""" File that uses PySerial to send information to the Arduino. Currently employs a manual interface that needs to be streamlined to recieve gcom input"""
 import time
 import serial
 
-## sketchy command line docs
-## python -m serial.tools.list_ports (shows all serial ports your computer has access to)
 
 # create instance of serial class from PySerial (current port is COM3 Arduino port)
 ser = serial.Serial(port="COM3", baudrate=9600)
 
-
 def flush_arduino():
-    """ 
+    """
     EFFECT: gets any pending responses from the Arduino and prints them, the chain of
     responses will always end at a prompt for more input. So to skip to the next input 
     prompt all that ever needs to be done is to flush out all pending strings.
@@ -24,7 +21,6 @@ def flush_arduino():
         value_str = str(value, "UTF-8")
         # if message is a string response from the Arduino
         print(value_str)
-
 
 # keep reading serial inputs
 while True:
